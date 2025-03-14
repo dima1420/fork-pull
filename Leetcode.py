4cb7ccaa729dda8cb9e883539a5265829fd679eb
dima1420 <sayfiyevvdiyorbekk@gmail.com>
2025-03-14 20:18:29 +0500

    leetcode

---
+++ Leetcode.py
@@ -0,0 +1,17 @@
+# a=11
+# b=bin(a)
+# print(bin(a))
+# print(b[2:])
+# uppercase_letters = [chr(i) for i in range(65, 91)]
+# print(len(uppercase_letters))
+# print(uppercase_letters)
+# print(uppercase_letters.index("Y"))
+
+# print(chr(90))
+# print(30%7)
+# print(chr(65))
+# print(ord('@'))
+# print(52//26)
+print(int('11',2))
+print(int('1',2))
+print(bin(4))
